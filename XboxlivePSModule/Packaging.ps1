35b3b7c2bb7926191f17bb901a0b371627854cdf
Paul Liu <jicailiu@microsoft.com>
2017-05-30 16:22:54 -0700

    Update build script for new doc structure. (#9)

--- XboxlivePSModule/Packaging.ps1
+++ XboxlivePSModule/Packaging.ps1
@@ -1,20 +1,37 @@
+param([string]$binarySrcRoot, [string]$sourceSrcRoot, [string]$destRoot)
+
 #only do the job for release binaries
 if($env:BUILDCONFIGURATION -ine "release") 
 {
     return
 }
 
-#create a package folder
-$src = Join-Path $env:XES_OUTDIR "bin\XboxLiveCmdlets\*"
-$dest = Join-Path $env:XES_DFSDROP "ModulePackage"
+if ([string]::IsNullOrEmpty($binarySrcRoot))
+{
+    $binarySrcRoot = $env:XES_OUTDIR
+}
 
+if ([string]::IsNullOrEmpty($destRoot))
+{
+    $destRoot = $env:XES_DFSDROP
+}
+
+if ([string]::IsNullOrEmpty($sourceSrcRoot))
+{
+    $sourceSrcRoot = $env:BUILD_SOURCESDIRECTORY
+}
+
+$src = Join-Path $binarySrcRoot "bin\XboxLiveCmdlets\*"
+$dest = Join-Path $destRoot "ModulePackage"
+
+#create a package folder
 New-Item $dest -type directory
 
 Write-Output "copying $src => $dest"
 Copy-Item  $src $dest -Force 
-$psdSrcFolder = Join-Path $env:BUILD_SOURCESDIRECTORY "XboxlivePSModule\"
+$psdSrcFolder = Join-Path $sourceSrcRoot "XboxlivePSModule\"
 $psdSrcPath = Join-Path $psdSrcFolder "XboxLivePsModule.psd1"
-$helpSrcPath = Join-Path $psdSrcFolder "XboxLivePsModule_87868831-0094-40ae-94e5-e740bef0c5f0_HelpInfo.xml"
+$helpSrcPath = Join-Path $sourceSrcRoot "docs\en-US\XboxLiveCmdlet.dll-help.xml"
 Copy-Item $psdSrcPath $dest
 Copy-Item $helpSrcPath $dest
 
