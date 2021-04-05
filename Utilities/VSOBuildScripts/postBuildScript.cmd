fad35cd34d559894ad589e83b310e79a84cf432c
Thomas Culotta <tculotta@microsoft.com>
2021-04-05 15:14:48 -0700

    matching output directories to those build machine

--- Utilities/VSOBuildScripts/postBuildScript.cmd
+++ Utilities/VSOBuildScripts/postBuildScript.cmd
@@ -1,23 +1,13 @@
-if "%1" == "local" goto testlocal
-goto start
-
-
-:testlocal
-set TOOLS_SOURCEDIRECTORY=%CD%\..\..
-goto serializeForPostbuild
-
-
-:start
 echo Running postBuildScript.cmd
 echo on
 
 set
 
+set TOOLS_BINARIESDIRECTORY=%BUILD_BINARIESDIRECTORY%
 set TOOLS_SOURCEDIRECTORY=%BUILD_SOURCESDIRECTORY%
 
 
-:serializeForPostbuild
-set TOOLS_DROP_LOCATION=%TOOLS_SOURCEDIRECTORY%\XboxLiveDeveloperTools
+set TOOLS_DROP_LOCATION=%TOOLS_BINARIESDIRECTORY%\XboxLiveDeveloperTools
 rmdir /s /q %TOOLS_DROP_LOCATION%
 mkdir %TOOLS_DROP_LOCATION%
 mkdir %TOOLS_DROP_LOCATION%\ToolZip
@@ -40,13 +30,14 @@ set LONG_SDK_RELEASE_NAME=%SDK_RELEASE_NAME%-%SDK_POINT_NAME_YEAR%%SDK_POINT_NAM
 
 
 REM ------------------- TOOLS BEGIN -------------------
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblConfig\bin\Release\XblConfig.exe                     %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\GlobalStorage\bin\Release\GlobalStorage.exe             %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblDevAccount\bin\Release\XblDevAccount.exe             %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblPlayerDataReset\bin\Release\XblPlayerDataReset.exe   %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblConnectedStorage\bin\Release\XblConnectedStorage.exe %TOOLS_DROP_LOCATION%\ToolZip
+set TOOLS_RELEASEDIRECTORY=%TOOLS_BINARIESDIRECTORY%\Release\anycpu\bin
 
-copy %TOOLS_SOURCEDIRECTORY%\Tools\MultiplayerSessionHistoryViewer\bin\Release\MultiplayerSessionHistoryViewer.exe %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblConfig\XblConfig.exe                                  %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\GlobalStorage\GlobalStorage.exe                          %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblDevAccount\XblDevAccount.exe                          %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblPlayerDataReset\XblPlayerDataReset.exe                %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblConnectedStorage\XblConnectedStorage.exe              %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\SessionHistoryViewer\MultiplayerSessionHistoryViewer.exe %TOOLS_DROP_LOCATION%\ToolZip
 
 
 %TOOLS_SOURCEDIRECTORY%\Utilities\VSOBuildScripts\vZip.exe /FOLDER:%TOOLS_DROP_LOCATION%\ToolZip /OUTPUTNAME:%TOOLS_DROP_LOCATION%\XboxLiveDeveloperTools-%LONG_SDK_RELEASE_NAME%.zip
@@ -56,5 +47,3 @@ echo.
 echo Done postBuildScript.cmd
 echo.
 endlocal
-
-:done
