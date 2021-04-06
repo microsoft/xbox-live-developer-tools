815f760f5e1b8fe5a88fa5ce7ba3a454edbf222c
tculotta <60450686+tculotta@users.noreply.github.com>
2021-04-05 17:08:03 -0700

    matching output directories to those build machine (#85)
    * updated directories in postbuildscript
    
    * matching output directories to those build machine

--- Utilities/VSOBuildScripts/postBuildScript.cmd
+++ Utilities/VSOBuildScripts/postBuildScript.cmd
@@ -1,22 +1,20 @@
-if "%1" == "local" goto testlocal
-goto start
-
-:start
-:testlocal
-set TOOLS_SOURCEDIRECTORY=%CD%\..\..
-
-:serializeForPostbuild
-
 echo Running postBuildScript.cmd
 echo on
 
-set TOOLS_DROP_LOCATION=%TOOLS_SOURCEDIRECTORY%\XboxLiveDeveloperTools
+set
+
+set TOOLS_BINARIESDIRECTORY=%BUILD_BINARIESDIRECTORY%
+set TOOLS_SOURCEDIRECTORY=%BUILD_SOURCESDIRECTORY%
+
+
+set TOOLS_DROP_LOCATION=%TOOLS_BINARIESDIRECTORY%\XboxLiveDeveloperTools
 rmdir /s /q %TOOLS_DROP_LOCATION%
 mkdir %TOOLS_DROP_LOCATION%
 mkdir %TOOLS_DROP_LOCATION%\ToolZip
 
 setlocal
-call %CD%\setBuildVersion.cmd
+call %TOOLS_SOURCEDIRECTORY%\Utilities\VSOBuildScripts\setBuildVersion.cmd
+
 
 REM ------------------- VERSION SETUP BEGIN -------------------
 for /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
@@ -32,20 +30,20 @@ set LONG_SDK_RELEASE_NAME=%SDK_RELEASE_NAME%-%SDK_POINT_NAME_YEAR%%SDK_POINT_NAM
 
 
 REM ------------------- TOOLS BEGIN -------------------
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblConfig\bin\Release\XblConfig.exe                     %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\GlobalStorage\bin\Release\GlobalStorage.exe             %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblDevAccount\bin\Release\XblDevAccount.exe             %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblPlayerDataReset\bin\Release\XblPlayerDataReset.exe   %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\CommandLine\XblConnectedStorage\bin\Release\XblConnectedStorage.exe %TOOLS_DROP_LOCATION%\ToolZip
+set TOOLS_RELEASEDIRECTORY=%TOOLS_BINARIESDIRECTORY%\Release\anycpu\bin
 
-copy %TOOLS_SOURCEDIRECTORY%\Tools\MultiplayerSessionHistoryViewer\bin\Release\MultiplayerSessionHistoryViewer.exe %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblConfig\XblConfig.exe                                  %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\GlobalStorage\GlobalStorage.exe                          %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblDevAccount\XblDevAccount.exe                          %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblPlayerDataReset\XblPlayerDataReset.exe                %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblConnectedStorage\XblConnectedStorage.exe              %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\SessionHistoryViewer\MultiplayerSessionHistoryViewer.exe %TOOLS_DROP_LOCATION%\ToolZip
 
-%CD%\vZip.exe /FOLDER:%TOOLS_DROP_LOCATION%\ToolZip /OUTPUTNAME:%TOOLS_DROP_LOCATION%\XboxLiveTools-%LONG_SDK_RELEASE_NAME%.zip
+
+%TOOLS_SOURCEDIRECTORY%\Utilities\VSOBuildScripts\vZip.exe /FOLDER:%TOOLS_DROP_LOCATION%\ToolZip /OUTPUTNAME:%TOOLS_DROP_LOCATION%\XboxLiveDeveloperTools-%LONG_SDK_RELEASE_NAME%.zip
 rmdir /s /q %TOOLS_DROP_LOCATION%\ToolZip
 
 echo.
 echo Done postBuildScript.cmd
 echo.
 endlocal
-
-:done
