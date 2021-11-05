d5fdd84d3ccf3f39516b05c5478633b91832f73b
Nathan Iskandar <natiskan@microsoft.com>
2021-11-05 01:48:03 -0700

    fix postbuildscript.cmd to move the correct files instead of sdk.tools files

--- Utilities/Pipelines/Scripts/postBuildScript.cmd
+++ Utilities/Pipelines/Scripts/postBuildScript.cmd
@@ -3,26 +3,16 @@ echo on
 
 set
 
-dir /s /b %BUILD_BINARIESDIRECTORY%
-dir /s /b %BUILD_SOURCESDIRECTORY%
-
 set TOOLS_BINARIESDIRECTORY=%BUILD_BINARIESDIRECTORY%
 set TOOLS_SOURCEDIRECTORY=%BUILD_SOURCESDIRECTORY%
 
+
 set TOOLS_DROP_LOCATION=%TOOLS_BINARIESDIRECTORY%\XboxLiveDeveloperTools
-echo %TOOLS_DROP_LOCATION%
-dir /s /b %TOOLS_DROP_LOCATION%
-rmdir /s /q %TOOLS_DROP_LOCATION%
-dir /s /b %TOOLS_DROP_LOCATION%
-
 set TOOLS_DROP_LOCATION_VPACK=%TOOLS_DROP_LOCATION%\Tools-VPack
-echo %TOOLS_DROP_LOCATION_VPACK%
-
+rmdir /s /q %TOOLS_DROP_LOCATION%
 mkdir %TOOLS_DROP_LOCATION%
 mkdir %TOOLS_DROP_LOCATION_VPACK%
 mkdir %TOOLS_DROP_LOCATION%\ToolZip
-dir /s /b %TOOLS_DROP_LOCATION%
-dir /s /b %TOOLS_DROP_LOCATION%\ToolZip
 
 setlocal
 call %TOOLS_SOURCEDIRECTORY%\Utilities\Pipelines\Scripts\setBuildVersion.cmd
@@ -42,28 +32,20 @@ set LONG_SDK_RELEASE_NAME=%SDK_RELEASE_NAME%-%SDK_POINT_NAME_YEAR%%SDK_POINT_NAM
 
 
 REM ------------------- TOOLS BEGIN -------------------
-set TOOLS_RELEASEDIRECTORY=%TOOLS_BINARIESDIRECTORY%\%CONFIGURATION%\AnyCPU\bin
+set TOOLS_RELEASEDIRECTORY=%TOOLS_BINARIESDIRECTORY%\%CONFIGURATION%\anycpu\bin
 
-copy %TOOLS_SOURCEDIRECTORY%\Tools\ThirdPartyNotices.txt %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\Tools\WinHttpFiddlerOff.cmd %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_SOURCEDIRECTORY%\Tools\WinHttpFiddlerOn.cmd  %TOOLS_DROP_LOCATION%\ToolZip
-
-copy %TOOLS_RELEASEDIRECTORY%\XblPCSandbox\XblPCSandbox.exe            %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_RELEASEDIRECTORY%\XblTestAccountGui\XblTestAccountGui.exe  %TOOLS_DROP_LOCATION%\ToolZip
-copy %TOOLS_RELEASEDIRECTORY%\XblTestAccountGui\XblTestAccountGui.docx %TOOLS_DROP_LOCATION%\ToolZip
-
-robocopy /NJS /NJH /MT:16 /S /NP %TOOLS_SOURCEDIRECTORY%\Tools\XceTool         %TOOLS_DROP_LOCATION%\ToolZip\XceTool
-robocopy /NJS /NJH /MT:16 /S /NP %TOOLS_SOURCEDIRECTORY%\Tools\MatchSimTool    %TOOLS_DROP_LOCATION%\ToolZip\MatchSimTool
-robocopy /NJS /NJH /MT:16 /S /NP %TOOLS_SOURCEDIRECTORY%\Tools\XboxLiveCompute %TOOLS_DROP_LOCATION%\ToolZip\XboxLiveCompute
+copy %TOOLS_RELEASEDIRECTORY%\XblConfig\XblConfig.exe                                  %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\GlobalStorage\GlobalStorage.exe                          %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblDevAccount\XblDevAccount.exe                          %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblPlayerDataReset\XblPlayerDataReset.exe                %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\XblConnectedStorage\XblConnectedStorage.exe              %TOOLS_DROP_LOCATION%\ToolZip
+copy %TOOLS_RELEASEDIRECTORY%\SessionHistoryViewer\MultiplayerSessionHistoryViewer.exe %TOOLS_DROP_LOCATION%\ToolZip
 
 REM ------------------- OS VPACK BEGIN -------------------
-copy %TOOLS_RELEASEDIRECTORY%\XblPCSandbox\XblPCSandbox.exe           %TOOLS_DROP_LOCATION_VPACK%
-copy %TOOLS_RELEASEDIRECTORY%\XblTestAccountGui\XblTestAccountGui.exe %TOOLS_DROP_LOCATION_VPACK%
-
-
-%TOOLS_SOURCEDIRECTORY%\Utilities\Pipelines\Scripts\vZip.exe /FOLDER:%TOOLS_DROP_LOCATION%\ToolZip /OUTPUTNAME:%TOOLS_DROP_LOCATION%\XboxLiveTools-%LONG_SDK_RELEASE_NAME%.zip
-rmdir /s /q %TOOLS_DROP_LOCATION%\ToolZip
-
+copy %TOOLS_RELEASEDIRECTORY%\XblConfig\XblConfig.exe                                  %TOOLS_DROP_LOCATION_VPACK%
+copy %TOOLS_RELEASEDIRECTORY%\XblConnectedStorage\XblConnectedStorage.exe              %TOOLS_DROP_LOCATION_VPACK%
+copy %TOOLS_RELEASEDIRECTORY%\XblDevAccount\XblDevAccount.exe                          %TOOLS_DROP_LOCATION_VPACK%
+copy %TOOLS_RELEASEDIRECTORY%\XblPlayerDataReset\XblPlayerDataReset.exe                %TOOLS_DROP_LOCATION_VPACK%
 
 echo.
 echo Done postBuildScript.cmd
